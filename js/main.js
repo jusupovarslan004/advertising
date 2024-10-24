6e8725b6f458b1d17fa033636341b06c14f02fdb
Arslan Jusupov <jusupovarslan04@gmail.com>
2024-10-24 14:20:18 +0600

    fixed text slide

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.getElementById('learnMoreBtn').addEventListener('click', function () {
     document.querySelector('.hero').style.display = 'none';
     // Показать остальные секции
     document.getElementById('about').classList.remove('hidden');
+    document.getElementById('images').classList.remove('hidden');
     document.getElementById('products').classList.remove('hidden');
     document.getElementById('reviews').classList.remove('hidden');
     document.getElementById('contacts').classList.remove('hidden');
